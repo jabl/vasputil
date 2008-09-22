94e5f73294e7f31f76ca4d6869b51d60acbdf449
Janne Blomqvist <Janne.Blomqvist@tkk.fi>
2008-09-22 14:02:46 +0300

    Bump version to 4.3.

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@
 from distutils.core import setup
 
 setup(name='vasputil',
-      version='4.2',
+      version='4.3',
       description='VASP utilities',
       author='Janne Blomqvist',
       author_email='Janne.Blomqvist@tkk.fi',
       url='http://www.fyslab.hut.fi/~job/',
       packages=['vasputil'],
       package_dir = {'': 'lib/python'},
-      scripts = ['bin/vasputil_dosplot', 'bin/vasputil_interpolate', 'bin/vasputil_zlayers']
+      scripts = ['bin/vasputil_atomsdistance', \
+              'bin/vasputil_dosplot', \
+              'bin/vasputil_interpolate', \
+              'bin/vasputil_zlayers']
      )
 
