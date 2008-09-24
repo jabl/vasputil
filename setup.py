13eba00c2f41c6a9dda9c131bdd5277469574cfc
Janne Blomqvist <blomqvist.janne@gmail.com>
2008-09-24 19:12:41 +0300

    New python utility.

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ setup(name='vasputil',
       packages=['vasputil'],
       package_dir = {'': 'lib/python'},
       scripts = ['bin/vasputil_atomsdistance', \
+              'bin/vasputil_atomsmoved', \
+              'bin/vasputil_direct2cartesian', \
               'bin/vasputil_dosplot', \
               'bin/vasputil_interpolate', \
               'bin/vasputil_poscar2xyz', \
