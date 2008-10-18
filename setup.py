01375424093d72e28dea29bf857e0500d9bbd547
Janne Blomqvist <Janne.Blomqvist@tkk.fi>
2008-10-18 14:46:21 +0300

    update setup.py to new dir structure.

--- setup.py
+++ setup.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python
 
 from distutils.core import setup
+from glob import glob
+
+scripts = glob('scripts/*')
 
 setup(name='vasputil',
-      version='4.3',
+      version='5.0',
       description='VASP utilities',
       author='Janne Blomqvist',
       author_email='Janne.Blomqvist@tkk.fi',
       url='http://www.fyslab.hut.fi/~job/',
       packages=['vasputil'],
-      package_dir = {'': 'lib/python'},
-      scripts = ['bin/vasputil_atomsdistance', \
-              'bin/vasputil_atomsmoved', \
-              'bin/vasputil_direct2cartesian', \
-              'bin/vasputil_dosplot', \
-              'bin/vasputil_interpolate', \
-              'bin/vasputil_nearestneighbors', \
-              'bin/vasputil_poscar2xyz', \
-              'bin/vasputil_zlayers']
-     )
+#      package_dir = {'': 'lib/python'},
+      scripts = scripts)
 
