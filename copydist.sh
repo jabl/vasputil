66db64d1cebad9ef0c1209e4dffbb61216126d04
Janne Blomqvist <Janne.Blomqvist@tkk.fi>
2008-11-16 20:03:55 +0200

    Don't create generated changelog in public_html.

--- copydist.sh
+++ copydist.sh
@@ -5,5 +5,4 @@ git2cl > ChangeLog
 ./setup.py sdist
 cp dist/vasputil-*.tar.gz ~/public_html/vasputil
 cp doc/README.html ~/public_html/vasputil
-git log --name-status > ~/public_html/vasputil/gitlog
 chmod a+r ~/public_html/vasputil/*
