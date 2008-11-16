35dcb04d6e06e1207fe60efcee98d3a8a2a7fe61
Janne Blomqvist <blomqvist.janne@gmail.com>
2008-11-16 19:30:25 +0200

    Fixups, documentation for 5.0 release.

--- copydist.sh
+++ copydist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-make dist
-make doc
-cp vasputil-*.tar.gz ~/public_html/vasputil
+rst2html README doc/README.html
+./setup.py sdist
+cp dist/vasputil-*.tar.gz ~/public_html/vasputil
 cp doc/README.html ~/public_html/vasputil
 chmod a+r ~/public_html/vasputil/*
--- setup.py
+++ setup.py
@@ -12,6 +12,5 @@ setup(name='vasputil',
       author_email='Janne.Blomqvist@tkk.fi',
       url='http://www.fyslab.hut.fi/~job/',
       packages=['vasputil'],
-#      package_dir = {'': 'lib/python'},
       scripts = scripts)
 
