a64ef9a07924de40bb614759f1ae4f4e23afbe7c
Janne Blomqvist <blomqvist.janne@gmail.com>
2008-11-16 19:59:57 +0200

    Changelog fixes, generate automagically.

--- copydist.sh
+++ copydist.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+# Make the dist directory
 rst2html README doc/README.html
+git2cl > ChangeLog
 ./setup.py sdist
 cp dist/vasputil-*.tar.gz ~/public_html/vasputil
 cp doc/README.html ~/public_html/vasputil
+git log --name-status > ~/public_html/vasputil/gitlog
 chmod a+r ~/public_html/vasputil/*
