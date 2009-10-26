254c11bc1502b0296db7a609caebcb0d8c4cdb68
Janne Blomqvist <Janne.Blomqvist@tkk.fi>
2009-10-26 16:50:09 +0200

    Update to version number of current development

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from glob import glob
 scripts = glob('scripts/*[a-z,1-9]')
 
 setup(name='vasputil',
-      version='5.2',
+      version='5.3',
       description='VASP utilities',
       author='Janne Blomqvist',
       author_email='Janne.Blomqvist@tkk.fi',
