45388021e2d872f7753f2626f7b63c5546c6f58a
Janne Blomqvist <Janne.Blomqvist@tkk.fi>
2010-09-09 01:37:36 +0300

    Create README.html in root dir

--- copydist.sh
+++ copydist.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Make the dist directory
-rst2html README doc/README.html
+rst2html README README.html
 git2cl > ChangeLog
 ./setup.py sdist
 cp dist/vasputil-*.tar.gz ~/public_html/vasputil
-cp doc/README.html ~/public_html/vasputil
+cp README.html ~/public_html/vasputil
 chmod a+r ~/public_html/vasputil/*
