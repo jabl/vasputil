8f958c1741be992c6110f5b40f43c31567feddbf
Janne Blomqvist <Janne.Blomqvist@aalto.fi>
2012-07-20 10:59:00 +0300

    Change license to MIT, version 5.7.
    Vasputil is now available under the terms of the MIT (Expat)
    license. The MIT license is a simple and permissive license.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from glob import glob
 scripts = glob('scripts/*[a-z,1-9]')
 
 setup(name='vasputil',
-      version='5.6',
+      version='5.7',
       description='VASP utilities',
       author='Janne Blomqvist',
       author_email='Janne.Blomqvist@aalto.fi',
