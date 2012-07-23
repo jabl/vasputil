e2e2a0ac1d36c304f6dffc7c15d37ff17225a23e
Janne Blomqvist <Janne.Blomqvist@aalto.fi>
2012-07-23 08:49:36 +0300

    Change version to master branch

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from glob import glob
 scripts = glob('scripts/*[a-z,1-9]')
 
 setup(name='vasputil',
-      version='5.7',
+      version='master',
       description='VASP utilities',
       author='Janne Blomqvist',
       author_email='Janne.Blomqvist@aalto.fi',
